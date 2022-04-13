182b3cd42264f389505ac7378cec81f3bb3e5a6d
wyu <425487198@qq.com>
2022-04-13 18:20:06 +0800

    first commit

---
+++ src/main/java/com/lsbh/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lsbh;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld1!");
+        System.out.println("HelloWorld2!");
+        System.out.println("HelloWorld3!");
+        System.out.println("HelloWorld4!");
+        System.out.println("HelloWorld5!");
+    }
+}
